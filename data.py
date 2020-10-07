1134b31b6e3e3484916f71d24188a99e7c94dc48
Shingai Thornton <rsthornton@gmail.com>
2020-10-06 17:42:03 -0700

    Add files via upload

--- data.py
+++ data.py
@@ -1,85 +1,100 @@
 """ 
 A script to identify the most retweeted tweets from curated user lists
 """
-import tweepy
 import pandas as pd
 import json
+import config
 
-# Connect to Twitter API
+"""
+# Grab  data from twitter and dump to text files.
+"""
 
-auth = tweepy.OAuthHandler("*****", "*****" )
-auth.set_access_token("*****", "*****")
-api = tweepy.API(auth)
+class GetData(object):
 
-# Store tweet timelines from list members as a dictionary of "ResultSet" objects
+    def __init__(self):
+        pass
 
-the_lists =  {"gaming" : "Blockchain-gaming", "gurus": "Cryptogurus", "eth" : "ETH", "econ" : "Economics", "ai": "AI", "btc" :  "Bitcoin", "space" :  "Space", "systems" : "Systems"}
 
-for slug in the_lists.keys():
-    the_lists[slug] = api.list_timeline(screen_name='@shingaithornton', slug = the_lists[slug], owner_screen_name='@shingaithornton', include_rts = 'false', count = 5000)
-    
-# Isolate json of tweepy "status" objects, add them into a list of dictionaries
+    def get_tweets(self, listname, listcollection):
+        for each_list in config.the_lists.values():
+            for each_json_tweet in each_list:
+                if each_list == config.the_lists[listname]:
+                    listcollection.append(each_json_tweet._json)
 
-gaming_tweets, gurus_tweets, eth_tweets, econ_tweets, ai_tweets, btc_tweets, space_tweets, systems_tweets = [],[],[],[],[],[],[],[]
 
-def get_tweets(listname, listcollection):
-    for each_list in the_lists.values():
-        for each_json_tweet in each_list:
-            if each_list == the_lists[listname]:
-                listcollection.append(each_json_tweet._json)
+    def make_txt(self, listname, tweets):
+        with open('tweet_json_' + str(listname) + '.txt', 'w') as file:
+            file.write(json.dumps(tweets, indent=4))
 
-get_tweets('gaming', gaming_tweets), get_tweets('gurus', gurus_tweets), get_tweets('eth', eth_tweets), get_tweets('econ', econ_tweets)
-get_tweets('ai', ai_tweets), get_tweets('btc', btc_tweets), get_tweets('space', space_tweets), get_tweets('systems', systems_tweets) 
-                  
-# Write dictionaries of tweets into text files
-                  
-def make_txt(listname, tweets):
-    with open('tweet_json_' + str(listname) + '.txt', 'w') as file:
-        file.write(json.dumps(tweets, indent=4))
-            
-make_txt('gaming', gaming_tweets), make_txt('gurus', gurus_tweets), make_txt('eth', eth_tweets), make_txt('econ', econ_tweets),
-make_txt('ai', ai_tweets), make_txt('btc', btc_tweets), make_txt('space', space_tweets), make_txt('systems', systems_tweets)
-    
-# Set up dataframes from text files
 
-gaming_list, gurus_list, eth_list, econ_list, ai_list, btc_list, space_list, systems_list  = [],[],[],[],[],[],[],[]  
+"""
+# Set up dataframes object from text files
+"""
 
-def make_list(listname, textfile):
-    
-   with open(textfile, encoding='utf-8') as json_file:
-        all_data = json.load(json_file)
-        for each_dictionary in all_data:
-            text = each_dictionary['text']
-            retweet_count = each_dictionary['retweet_count']
-            favorite_count = each_dictionary['favorite_count']
-            user_name = each_dictionary['user']['name']
-            created_at = each_dictionary['created_at']
-            
-            try:
-                url = each_dictionary['entities']['urls'][0]['url']
-            except:
-                url = 'none'
-            
-            listname.append({'text': str(text),
-                                 'retweet_count': int(retweet_count),
-                                 'url': str(url),
-                                 'combo': (int(retweet_count) + int(favorite_count)),
-                                 'user_name': str(user_name),
-                                 'created_at': str(created_at)
-                                })  
+class DataFrames(object):
 
-make_list(gaming_list, 'tweet_json_gaming.txt'), make_list(gurus_list, 'tweet_json_gurus.txt'), make_list(eth_list, 'tweet_json_eth.txt'), make_list(econ_list, 'tweet_json_econ.txt'),
-make_list(ai_list, 'tweet_json_ai.txt'), make_list(btc_list, 'tweet_json_btc.txt'), make_list(space_list, 'tweet_json_space.txt'), make_list(systems_list, 'tweet_json_systems.txt')
-   
-# Create dataframes
+    def __init__(self):
+        pass
 
-def make_DF(listname):
-    listname =  pd.DataFrame(listname, columns = 
-                           ['text', 'retweet_count', 'combo', 'url', 'user_name', 'created_at'])
-    listname = listname.nlargest(15, ['retweet_count'])
-    return listname
+    def make_list(self, listname, textfile):
+       with open(textfile, encoding='utf-8') as json_file:
+            all_data = json.load(json_file)
+            for each_dictionary in all_data:
+                text = each_dictionary['text']
+                retweet_count = each_dictionary['retweet_count']
+                favorite_count = each_dictionary['favorite_count']
+                user_name = each_dictionary['user']['name']
+                created_at = each_dictionary['created_at']
 
-eth_df, gaming_df, gurus_df, econ_df, ai_df, btc_df, space_df, systems_df = make_DF(eth_list), make_DF(gaming_list), make_DF(gurus_list), make_DF(econ_list), make_DF(ai_list), make_DF(btc_list), make_DF(space_list), make_DF(systems_list)
+                try:
+                    url = each_dictionary['entities']['urls'][0]['url']
+                except:
+                    url = 'none'
+
+                listname.append({'text': str(text),
+                                     'retweet_count': int(retweet_count),
+                                     'url': str(url),
+                                     'combo': (int(retweet_count) + int(favorite_count)),
+                                     'user_name': str(user_name),
+                                     'created_at': str(created_at)
+                                    })
+
+
+    def make_DF(self, listname):
+        listname =  pd.DataFrame(listname, columns =
+                               ['text', 'retweet_count', 'combo', 'url', 'user_name', 'created_at'])
+        listname = listname.nlargest(15, ['retweet_count'])
+        return listname
+
+
+"""
+This code is still largely a mess but it is able to get the basic app working. Will definitely be cleaned up later.
+"""
+
+GetData().get_tweets('gaming', config.gaming_tweets), GetData().get_tweets('gurus', config.gurus_tweets), GetData().get_tweets('eth', config.eth_tweets), GetData().get_tweets('econ', config.econ_tweets)
+GetData().get_tweets('ai', config.ai_tweets), GetData().get_tweets('btc', config.btc_tweets), GetData().get_tweets('space', config.space_tweets), GetData().get_tweets('systems', config.systems_tweets)
+GetData().get_tweets('cryptofunds', config.cryptofunds_tweets), GetData().get_tweets('dao', config.dao_tweets), GetData().get_tweets('defi', config.defi_tweets), GetData().get_tweets('network', config.network_tweets)
+GetData().get_tweets('politicaleconomy', config.politicaleconomy_tweets), GetData().get_tweets('psych', config.psych_tweets), GetData().get_tweets('techexecs', config.techexecs_tweets), GetData().get_tweets('vc', config.vc_tweets)
+
+
+GetData().make_txt('gaming', config.gaming_tweets), GetData().make_txt('gurus', config.gurus_tweets), GetData().make_txt('eth', config.eth_tweets), GetData().make_txt('econ', config.econ_tweets)
+GetData().make_txt('ai', config.ai_tweets), GetData().make_txt('btc', config.btc_tweets), GetData().make_txt('space', config.space_tweets), GetData().make_txt('systems', config.systems_tweets)
+GetData().make_txt('cryptofunds', config.cryptofunds_tweets), GetData().make_txt('dao', config.dao_tweets), GetData().make_txt('defi', config.defi_tweets), GetData().make_txt('network', config.network_tweets)
+GetData().make_txt('politicaleconomy', config.politicaleconomy_tweets), GetData().make_txt('psych', config.psych_tweets), GetData().make_txt('techexecs', config.techexecs_tweets), GetData().make_txt('vc', config.vc_tweets)
+
+
+
+DataFrames().make_list(config.gaming_list, 'tweet_json_gaming.txt'), DataFrames().make_list(config.gurus_list, 'tweet_json_gurus.txt'), DataFrames().make_list(config.eth_list, 'tweet_json_eth.txt'), DataFrames().make_list(config.econ_list, 'tweet_json_econ.txt'),
+DataFrames().make_list(config.ai_list, 'tweet_json_ai.txt'), DataFrames().make_list(config.btc_list, 'tweet_json_btc.txt'), DataFrames().make_list(config.space_list, 'tweet_json_space.txt'), DataFrames().make_list(config.systems_list, 'tweet_json_systems.txt')
+DataFrames().make_list(config.cryptofunds_list, 'tweet_json_cryptofunds.txt'), DataFrames().make_list(config.dao_list, 'tweet_json_dao.txt'), DataFrames().make_list(config.defi_list, 'tweet_json_defi.txt'), DataFrames().make_list(config.network_list, 'tweet_json_network.txt'), DataFrames().make_list(config.politicaleconomy_list, 'tweet_json_politicaleconomy.txt'),
+DataFrames().make_list(config.psych_list, 'tweet_json_psych.txt'), DataFrames().make_list(config.techexecs_list, 'tweet_json_techexecs.txt'), DataFrames().make_list(config.vc_list, 'tweet_json_vc.txt')
+
+
+
+eth_df, gaming_df, gurus_df, econ_df, ai_df, btc_df, space_df, systems_df, cryptofunds_df, dao_df, defi_df, network_df, politicaleconomy_df, psych_df, techexecs_df, vc_df = DataFrames().make_DF(config.eth_list), DataFrames().make_DF(config.gaming_list), DataFrames().make_DF(config.gurus_list), DataFrames().make_DF(config.econ_list), DataFrames().make_DF(config.ai_list), DataFrames().make_DF(config.btc_list), DataFrames().make_DF(config.space_list), DataFrames().make_DF(config.systems_list), DataFrames().make_DF(config.cryptofunds_list), DataFrames().make_DF(config.dao_list), DataFrames().make_DF(config.defi_list), DataFrames().make_DF(config.network_list), DataFrames().make_DF(config.politicaleconomy_list), DataFrames().make_DF(config.psych_list), DataFrames().make_DF(config.techexecs_list), DataFrames().make_DF(config.vc_list)
+
 
 eth_df.to_excel("ethexcel.xlsx"), gaming_df.to_excel("gamingexcel.xlsx"), gurus_df.to_excel("gurusexcel.xlsx"), econ_df.to_excel("econexcel.xlsx"),
-ai_df.to_excel("aiexcel.xlsx"), btc_df.to_excel("btcxcel.xlsx"), space_df.to_excel("spaceexcel.xlsx"), systems_df.to_excel("systemsexcel.xlsx")
+ai_df.to_excel("aiexcel.xlsx"), btc_df.to_excel("btcxcel.xlsx"), space_df.to_excel("spaceexcel.xlsx"), systems_df.to_excel("systemsexcel.xlsx"), cryptofunds_df.to_excel("cryptofundsexcel.xlsx"),
+dao_df.to_excel("daoexcel.xlsx"), defi_df.to_excel("defiexcel.xlsx"), network_df.to_excel("networkexcel.xlsx"), politicaleconomy_df.to_excel("politicaleconomyexcel.xlsx"),
+psych_df.to_excel("psychexcel.xlsx"), techexecs_df.to_excel("techexecsexcel.xlsx"), vc_df.to_excel("vcexcel.xlsx")
--- web.py
+++ web.py
@@ -1,12 +1,18 @@
 "After using data.py to generate dataframes, use this to display them in a web broswer"
 
-
+import os
 from flask import render_template
-from flask import Flask
+from flask import Flask, jsonify
+from api import GetJSON
 import pandas as pd
+
 app = Flask(__name__)
 
-@app.route("/web")
+@app.route("/api")
+def get_api():
+    return jsonify(GetJSON().get_data())
+
+@app.route("/")
 def show_tables():
     eth = pd.read_excel('ethexcel.xlsx', header=0, delim_whitespace=True)
     gaming = pd.read_excel('gamingexcel.xlsx', header=0, delim_whitespace=True)
@@ -16,24 +22,50 @@ def show_tables():
     btc = pd.read_excel('btcxcel.xlsx', header=0, delim_whitespace=True)
     space = pd.read_excel('spaceexcel.xlsx', header=0, delim_whitespace=True)
     systems = pd.read_excel('systemsexcel.xlsx', header=0, delim_whitespace=True)
+    cryptofunds = pd.read_excel('cryptofundsexcel.xlsx', header=0, delim_whitespace=True)
+    dao = pd.read_excel('daoexcel.xlsx', header=0, delim_whitespace=True)
+    defi = pd.read_excel('defiexcel.xlsx', header=0, delim_whitespace=True)
+    network = pd.read_excel('networkexcel.xlsx', header=0, delim_whitespace=True)
+    politicaleconomy = pd.read_excel('politicaleconomyexcel.xlsx', header=0, delim_whitespace=True)
+    psych = pd.read_excel('psychexcel.xlsx', header=0, delim_whitespace=True)
+    techexecs = pd.read_excel('techexecsexcel.xlsx', header=0, delim_whitespace=True)
+    vc = pd.read_excel('vcexcel.xlsx', header=0, delim_whitespace=True)
+    
 
     eth.set_index(['url'], inplace=True,)
-    eth.index.name=None
+    eth.index.name="Ethereum"
     gaming.set_index(['url'], inplace=True,)
-    gaming.index.name=None
+    gaming.index.name=" Crypto Gaming"
     gurus.set_index(['url'], inplace=True,)
-    gurus.index.name=None
+    gurus.index.name="Cryptogurus"
     econ.set_index(['url'], inplace=True,)
-    econ.index.name=None
+    econ.index.name="Economics"
     ai.set_index(['url'], inplace=True,)
-    ai.index.name=None
+    ai.index.name="Artifcial Intelligence"
     btc.set_index(['url'], inplace=True,)
-    btc.index.name=None
+    btc.index.name="Bitcoin"
     space.set_index(['url'], inplace=True,)
-    space.index.name=None
+    space.index.name="Space"
     systems.set_index(['url'], inplace=True,)
-    systems.index.name=None
-    
+    systems.index.name="Systems"
+    cryptofunds.set_index(['url'], inplace=True,)
+    cryptofunds.index.name="Cryptofunds"
+
+    dao.set_index(['url'], inplace=True,)
+    dao.index.name="dao"
+    defi.set_index(['url'], inplace=True,)
+    defi.index.name="defi"
+    network.set_index(['url'], inplace=True,)
+    network.index.name="network"
+    politicaleconomy.set_index(['url'], inplace=True,)
+    politicaleconomy.index.name="politicaleconomy"
+    psych.set_index(['url'], inplace=True,)
+    psych.index.name="psych"
+    techexecs.set_index(['url'], inplace=True,)
+    techexecs.index.name="techexecs"
+    vc.set_index(['url'], inplace=True,)
+    vc.index.name="vc"
+
     eth_table = eth
     gaming_table = gaming
     gurus_table = gurus
@@ -42,19 +74,38 @@ def show_tables():
     btc_table = btc
     space_table = space
     systems_table = systems
-    
-    
+    cryptofunds_table = cryptofunds
+    dao_table = dao
+    defi_table = defi
+    network_table = network
+    politicaleconomy_table = politicaleconomy
+    psych_table = psych
+    techexecs_table = techexecs
+    vc_table = vc
+
+
     return render_template('view.html',tables=[eth_table.to_html(classes='text'),
                                                gaming_table.to_html(classes='text'),
-                                               gurus_table.to_html(classes='text'), 
-                                               econ_table.to_html(classes='text'), 
+                                               gurus_table.to_html(classes='text'),
+                                               econ_table.to_html(classes='text'),
                                                ai_table.to_html(classes='text'),
                                                btc_table.to_html(classes='text'),
                                                space_table.to_html(classes='text'),
-                                               systems_table.to_html(classes='text')],
-                                               titles = ['na', 'text'])
-    
-  
+                                               systems_table.to_html(classes='text'),
+                                               dao_table.to_html(classes='text'),
+                                               defi_table.to_html(classes='text'),
+                                               network_table.to_html(classes='text'),
+                                               politicaleconomy_table.to_html(classes='text'),
+                                               psych_table.to_html(classes='text'),
+                                               techexecs_table.to_html(classes='text'),
+                                               vc_table.to_html(classes='text')],
+                                               titles = ['na', 'Lists'])
+
+
 
 if __name__ == "__main__":
+    os.system("python data.py")
     app.run(debug=True)
+    # In order to run the following line, put together a bash file with a 'rm *.<file_type>'.
+    # Be sure the right permissions are set up for it --> chmod 755 <file_name>.sh
+    # os.system("./remove_files.sh")
