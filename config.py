041286455e710c8396a08efef8c14478d5ac3999
Shingai Thornton <rsthornton@gmail.com>
2020-10-17 10:53:28 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,3 @@
-"""
-import os
-import tweepy
-
-consumer_key = os.environ['KEY']
-consumer_secret = os.environ['SECRET']
-access_token_one = os.environ['ONE']
-access_token_two = os.environ['TWO']
-
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token_one, access_token_two)
-api = tweepy.API(auth)
-"""
-
 import os, tweepy
 
 api_key = os.environ['KEY']
